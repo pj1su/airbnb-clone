80e04db49bfc4ead6371c68e9caa9b733d7f6137
Donghun Jeong <s02@safehomesui-MacBookPro.local>
2022-01-26 15:43:33 +0900

    #14part of 1

--- rooms/views.py
+++ rooms/views.py
@@ -1,11 +1,11 @@
 from django.http import Http404
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, reverse
 
 # from django.urls import reverse
+from django.core.paginator import Paginator, EmptyPage
 
-# from django.core.paginator import Paginator, EmptyPage
 # from django.utils import timezone
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView, UpdateView, CreateView
 from . import models
 
 
@@ -27,11 +27,48 @@ class HomeView(ListView):
     # obj,page 들을 이 함수가 받아옴.
 
 
-def room_detail(request, pk):
-    try:
-        room = models.Room.objects.get(pk=pk)
-        return render(request, "rooms/detail.html", {"room": room})
-    except models.Room.DoesNotExist:
-        # return redirect(reverse("core:home"))
-        raise Http404()
-    # 404.html은 자동적으로 404에러가 인식을 하는데 templates파일에 들어있어야 인식 다른 폴더안
+# def room_detail(request, pk):
+#     try:
+#         room = models.Room.objects.get(pk=pk)
+#         return render(request, "rooms/detail.html", {"room": room})
+#     except models.Room.DoesNotExist:
+#         # return redirect(reverse("core:home"))
+#         raise Http404()
+# 404.html은 자동적으로 404에러가 인식을 하는데 templates파일에 들어있어야 인식 다른 폴더안
+
+
+class RoomDetail(DetailView):
+
+    """ RoomDetail Definition """
+
+    model = models.Room
+
+
+class EditRoomView(UpdateView):
+    model = models.Room
+    template_name = "rooms/room_edit.html"
+    fields = (
+        "name"
+        "description"
+        "country"
+        "price"
+        "city"
+        "address"
+        "guests"
+        "beds"
+        "bedrooms"
+        "baths"
+        "check_in"
+        "check_out"
+        "instant_book"
+        "room_type"
+        "amenity"
+        "facility"
+        "house_roul"
+    )
+
+    def get_object(self, queryset=None):
+        room = super().get_object(queryset=queryset)
+        if room.host.pk != self.request.user.pk:
+            raise Http404()
+        return room
--- users/views.py
+++ users/views.py
@@ -1,3 +1,13 @@
+from django.views import View
 from django.shortcuts import render
+from . import forms
 
-# Create your views here.
+# form_vaild는 HttpResponse형태이기떄문에 redirect-> reverse해야함
+class LoginView(View):
+    def get(self, request):
+        form = forms.LoginForm(initial={"email": "wltn@qkr.com"})  # 미리 입력되어있게
+        return render(request, "users/login.html", {"form": form})
+
+    def post(self, request):
+        form = forms.LoginForm(request.POST)
+        return render(request, "users/login.html", {"form": form})
