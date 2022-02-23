e547a0bea4f79359d6b5365881884ffddb60d51c
Jisu Park <wltn8223@gmail.com>
2022-02-23 23:11:21 +0900

    Postgresql

--- config/settings.py
+++ config/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = "ymer$6r&rkki8fi8c$5aield7pqhrx-g!=ji11@aif2&84*+2&"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(os.environ.get("DEBUG"))
 
 ALLOWED_HOSTS = "*"
 
@@ -87,13 +87,27 @@ WSGI_APPLICATION = "config.wsgi.application"
 
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
-
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+# env는 일라스틱빈스택에 올라가지않아서 aws에서 디버그를 찾으면 false로 찾을것이다 왜냐하면 commit이 되지않아서 deploy는 commit된걸 실행하기때문에
+# env파일은 manage.py로 호출하지만 일라스틱빈스택은 manage.py를 호출하지않는다
+if DEBUG:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+        }
     }
-}
+else:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.postgresql",
+            "HOST": os.environ.get("RDS_HOST"),
+            "NAME": os.environ.get("RDS_NAME"),
+            "USER": os.environ.get("RDS_USER"),
+            "PASSWORD": os.environ.get("RDS_PASSWORD"),
+            "PORT": "5432",
+        }
+    }
+    # 환경변수를 그래서 env에 안주고 EB환경변수 값으로 넣어줘서 일라스틱빈스택에 읽어오게함
 
 
 # Password validation
